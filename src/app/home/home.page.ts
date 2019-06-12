b7a2eb4c8f683a501d7c16f823d461b57be4b469
Luukas <luukki123@gmail.com>
2019-06-12 14:00:20 +0300

    Initial scaffold and stylings

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http2SecureServer } from 'http2';
+import { HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  login() {
+    this.http.get(
+      'https://accounts.spotify.com/authorize?client_id=86dab2ec88ef49cb98f1f308a7eecb7a&response_type=token&redirect_uri=localhost%3A8100%2Flist')
+      .subscribe(res => {
+      console.log(res);
+    });
+  }
 
 }
